1d7859375a6acf79616f93e0839982f9d755111a
NayotamaPradipta <n4y0t4m4@gmail.com>
2021-12-29 23:55:46 +0700

    Added more function definitions

--- ttt_function.cpp
+++ ttt_function.cpp
@@ -41,11 +41,40 @@ void start(){
 
 }
 
+void setPosition(){
+    while (!(cin >> position)){
+        cout << "Player " << player << ", please enter a valid number between 1 and 9: ";
+        cin.clear();
+        cin.ignore();
+    }
+    while (board[position-1] != " ") { // Player inputs a number that has been occupied with either an 'X' or 'O'
+        cout << "The particular board cell is occupied!\n\n";
+        cout << "Player " << player << ", please re-enter an empty/available cell (Enter 1-9): ";
+        cin >> position;
+        cout << "\n";
+    }
+}
 
 
+void updateBoard(){
+    // Function to insert the symbol depending on which player is currently playing
+    if (player % 2 == 1) {
+        board[position-1] = "✖";
+    } else {
+        board[position-1] = "⊙";
+    }
+}
 
+void changePlayer(){
+    if (player == 1) { 
+        player += 1; // Change to Player 2
+    } else {
+        player -= 1; // Revert back to Player 1
+    }
+}
 
 void drawBoard(){
+    // Function to draw the updated version of the board
     cout << "     |     |      \n";
     cout << "  " << board[0] << "  |  " << board[1] << "  |  " << board[2] << "\n";
     cout << "_____|_____|_____ \n";
@@ -56,5 +85,30 @@ void drawBoard(){
     cout << "  " << board[6] << "  |  " << board[7] << "  |  " << board[8] << "\n";
     cout << "     |     |      \n";
     cout << "\n";
- 
+}
+
+bool isNotDraw(){
+    // Check Horizontally
+    if (board[0] == board[1] && board[1] == board[2] && board[1] != " "){
+        if (board[1] == "✖"){
+            cout << "Player 1 Wins!\n";
+        } else {
+            cout << "Player 2 Wins!\n";
+        }
+        return true;
+    } else if (board[3] == board[4] && board[4] == board[5] && board[4] != " ") {
+        if (board[4] == "✖"){
+            cout << "Player 1 Wins!\n";
+        } else {
+            cout << "Player 2 Wins!\n";
+        }
+        return true;
+    } else if (board[6] == board[7] && board[7] == board[8] && board[7] != " ") {
+        if (board[7] == "✖"){
+            cout << "Player 1 Wins!\n";
+        } else {
+            cout << "Player 2 Wins!\n";
+        }
+    } 
+    // Check Vertically
 }
